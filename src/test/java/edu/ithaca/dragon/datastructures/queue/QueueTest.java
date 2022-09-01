df67a744807134a56a9dfec620f635b679d59f31
Toby Dragon <dr.toby.dragon@gmail.com>
2022-08-31 23:03:23 -0400

    first commit

---
+++ src/test/java/edu/ithaca/dragon/datastructures/queue/QueueTest.java
@@ -0,0 +1,15 @@
+package edu.ithaca.dragon.datastructures.queue;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class QueueTest {
+    @Test
+    public void simpleEnqueueDequeueTest(){
+        Queue<String> myQueue = null; // creates a null pointer to write tests for, you change this later
+        //TODO write some tests assuming myQueue works correctly
+        fail("No test written yet");
+    }
+
+}
---
+++ src/test/java/edu/ithaca/dragon/datastructures/stack/StackTest.java
@@ -0,0 +1,16 @@
+package edu.ithaca.dragon.datastructures.stack;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class StackTest {
+    
+    @Test
+    public void simplePushPopTest(){
+        Stack<String> myStack = null; // creates a null pointer to write tests for, you change this later
+        //TODO write tests assuming myStack works correctly
+        fail("No test written yet");
+    }
+
+}
